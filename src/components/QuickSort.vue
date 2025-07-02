520142a725691c9d6acc42db375c023bf92e897e
bee1an <beelan@yeah.net>
2025-07-02 13:09:59 +0800

    feat: quickSort

---
+++ src/components/QuickSort.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+import type { ItemType } from './BaseSort.vue'
+import BaseSort from './BaseSort.vue'
+
+const { config: _c } = defineProps<{
+  config?: {
+    waitTime?: number
+    length?: number
+  }
+}>()
+
+const config = computed(() => Object.assign({ length: 40, waitTime: 200 }, _c))
+
+let promise: Promise<void>, resolve: () => void, reject: () => void
+function wait(ms: number) {
+  ; ({ promise, resolve, reject } = Promise.withResolvers<void>())
+  setTimeout(resolve, ms)
+  return promise
+}
+
+async function run(arr: Ref<ItemType[]>) {
+  const classifyItems = async (items: ItemType[], start: number, end: number) => {
+    if (end - start < 1) {
+      items.slice(start, end + 1).forEach((item) => {
+        item.sorted = true
+      })
+      return
+    }
+
+    let flagIndex = start
+    const flagItem = items[start]
+    flagItem.active = true
+
+    for (let i = start; i <= end; i++) {
+      const err = Symbol('')
+      const returnable = await wait(config.value.waitTime).catch(() => err)
+      if (returnable === err)
+        return
+      if (flagItem.item > items[i].item) {
+        const lesser = items.splice(i, 1)
+        items.splice(start, 0, ...lesser)
+        flagIndex++
+      }
+    }
+
+    items[flagIndex].sorted = true
+    classifyItems(items, start, flagIndex - 1)
+    classifyItems(items, flagIndex + 1, end)
+  }
+
+  classifyItems(arr.value, 0, arr.value.length - 1)
+}
+
+const vm = getCurrentInstance()
+function changeRef(instance: InstanceType<typeof BaseSort>) {
+  vm!.exposed = instance
+}
+
+defineExpose<Pick<InstanceType<typeof BaseSort>, 'rerun'>>()
+</script>
+
+<template>
+  <BaseSort :ref="changeRef as any" :config="config" @run-sort="run" @befor-run="reject?.()" />
+</template>
---
+++ src/pages/quickSort/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+definePage({
+  meta: {
+    title: '快速排序(QuickSort)',
+  },
+})
+
+const quickSort = useTemplateRef('quickSort')
+
+const length = useStorage('length', 20)
+
+const waitTime = useStorage('waitTime', 100)
+</script>
+
+<template>
+  <SortWrapper v-model="waitTime" v-model:length="length" @rerun="quickSort?.rerun()">
+    <QuickSort ref="quickSort" :config="{ waitTime, length }" />
+  </SortWrapper>
+</template>
